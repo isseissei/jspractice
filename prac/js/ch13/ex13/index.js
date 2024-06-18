10812efd08c35aeb3c881e687b7874a242ff5058
Issei Honda <Issei.Honda@jp.ricoh.com>
2024-06-19 00:45:21 +0900

    おわり

--- prac/js/ch13/ex13/index.js
+++ prac/js/ch13/ex13/index.js
@@ -5,27 +5,14 @@ export async function* walk(rootPath) {
     const stats = await fs.stat(rootPath);
 
     if (stats.isDirectory()) {
-        // ディレクトリであれば
         yield { path: rootPath, isDirectory: true };
 
-        // ディレクトリ内のファイルとサブディレクトリを再帰的に探索
         const files = await fs.readdir(rootPath);
         for (const file of files) {
             const fullPath = path.join(rootPath, file);
-            yield* walk(fullPath); // 再帰呼び出し
+            yield* walk(fullPath);
         }
     } else {
-        // ファイルであれば
         yield { path: rootPath, isDirectory: false };
     }
 }
-
-// // Example usage:
-// const rootDirectory = '../'; // 実際のディレクトリパスに置き換える
-
-// // 非同期ジェネレータからファイル/ディレクトリ情報を取得してコンソールに出力する例
-// (async () => {
-//     for await (const entry of walk(rootDirectory)) {
-//         console.log(`Path: ${entry.path}, Is Directory: ${entry.isDirectory}`);
-//     }
-// })();
